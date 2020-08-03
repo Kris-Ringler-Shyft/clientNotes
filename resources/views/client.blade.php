4cca03858485f9e1c8b72b8a72de5b939bccdfc7
Kris Ringler <krisringler@kriss-air.attlocal.net>
2020-08-03 12:23:20 -0700

    updated notes and form to show notes attached to the proper client user can create note to 1 client - need to make it a tag and multiple

--- resources/views/client.blade.php
+++ resources/views/client.blade.php
@@ -8,15 +8,29 @@
 
         <div id="content">
             <h2>Client Name: {{$client->name}}</h2>
+            <pre>{{$client_notes}}</pre>
 
-            {{$client_notes}}
-            {{$notes}}
+            <ul>
+                 @foreach($notes as $note)
+                <li>{{$note->note_content}}</li>
+
+            </ul>
+
+            @endforeach
+
 
         </div>
+
+
         <h3>create note</h3>
         <form action="/create-note" method="post">
             <input type="text" name="note_title" placeholder="Title">
-            <input type="text" name="note_content" placeholder="Content">
+            <select name="clients_select">
+                @foreach($clients as $client)
+                     <option value="{{$client->id}}">{{$client->name}}</option>
+                @endforeach
+            </select>
+            <textarea type="text" name="note_content" placeholder="Content" ></textarea>
             {{csrf_field()}}
             <button type="submit">Create Note</button>
         </form>
